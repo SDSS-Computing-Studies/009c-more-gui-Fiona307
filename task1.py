adef795ea433e2fa639a3b5f4fbf6463ab02b35d
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-11-03 11:18:00 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,11 +7,19 @@ win = tk.Tk()
 
 import math
 
-trianglepic = PhotoImage(name="task1.png")
+trianglepic = PhotoImage(file="triangle.png")
 l1 = tk.Label(win, image=trianglepic)
+
 a_e = tk.Entry(win, width=10)
 b_e = tk.Entry(win, width=10)
 c_e = tk.Entry(win, width=10)
 h_e = tk.Entry(win, width=10)
+
+instruction = "Enter in as much information about the\ntriangle shown and I will calculate the area"
+e1 = tk.Entry(win, textvariable=instruction)
+
+
+b1 = tk.Button(win, text="Calculate!")
+
 
 win.mainloop()
