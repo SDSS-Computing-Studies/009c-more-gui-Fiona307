48de7ed0bf11d68e1d5cff1c6d65af4c8b8481aa
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-11-03 11:28:55 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -17,7 +17,19 @@ h_e = tk.Entry(win, width=10)
 
 output = StringVar()
 instruction = "Enter in as much information about the\ntriangle shown and I will calculate the area"
-e1 = tk.Entry(win, textvariable=instruction)
+output.set(instruction)
+e1 = tk.Entry(win, textvariable=output)
+
+def calculate():
+    a = a_e.get()
+    b = b_e.get()
+    c = c_e.get()
+    h = h_e.get()
+    if h == None:
+        s = (float(a)+float(b)+float(c))/2
+        area = math.sqrt(s*(s-float(a))*(s-float(b))*(s-float(c)))
+        e1.delete(0,END)
+        e1.insert(0,answer)
 
 
 b1 = tk.Button(win, text="Calculate!")
